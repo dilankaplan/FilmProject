b62f1a3e9fb541feb9f97b32103668b507192cda
dilankaplan <45143995+dilankaplan@users.noreply.github.com>
2021-08-19 00:38:04 +0300

    .

---
+++ project.js
@@ -0,0 +1,38 @@
+const form= document.getElementById("film-form");
+const titleElement =document.querySelector("#title");
+const directorElement =document.querySelector("#director");
+const urlElement =document.querySelector("#url");
+
+
+const ui = new UI();
+
+const storage = new Storage();
+
+eventListener();
+function eventListener(){
+    form.addEventListener("submit",addFilm);
+    document.addEventListener("DOMContentLoaded",function(){
+        let films =storage.getFilmFromStorage();
+        ui.loadAllFilms(films);
+    })
+
+}
+function addFilm(e){
+    const title = titleElement.value;
+    const director = directorElement.value;
+    const url = urlElement.value;
+
+    if(title === "" || director === "" || url === ""){
+       ui.displayMessages("Tüm alanları doldurunuz...","danger")
+    }
+    else{
+        const newFilm =new Film(title,director,url);
+        ui.displayMessages("Film başarıyla eklendi","success")
+        storage.addFilmToStorage(newFilm);
+
+        ui.addFilmToUI(newFilm);
+    }
+    ui.clearInputs(titleElement,directorElement,urlElement);
+
+    e.preventDefault();
+}
---
+++ storage.js
@@ -0,0 +1,21 @@
+function Storage(){
+
+}
+Storage.prototype.addFilmToStorage = function(newFilm){
+  let films = this.getFilmsFromStorage();
+  films.push(newFilm);
+
+  localStorage.setItem("films",JSON.stringify(films));
+}
+Storage.prototype.getFilmFromStorage= function(){
+    let films;
+    if(localStorage.getItem("films")=== null){
+         films=[];
+    }
+    else{
+        films=JSON.parse(localStorage.getItem("films"));
+
+    }
+    return films;
+
+}
